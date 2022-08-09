6d1c656f45eceba2c0d1fda821a31095f1103991
noah <noah.sigel@gmail.com>
2022-08-09 21:46:18 +0900

    Create yarn workspaces

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client";
 import express, { Request, Response, NextFunction } from "express";
 import { validateRequest } from "zod-express-middleware";
 import { z } from "zod";
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 const router = express.Router();
 
+const prisma = new PrismaClient();
+
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 	if (err instanceof ApiError) {
 		return res
@@ -19,8 +22,12 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 
 router.post(
 	"/account",
-	validateRequest({ query: z.object({ email: z.string() }) }),
-	(req, res) => {
-		const payload = req.body();
+	validateRequest({ body: z.object({ address: z.string().optional() }) }),
+	async (req, res) => {
+		const payload = req.body.address;
+
+		if (!payload) return await prisma;
 	}
 );
+
+console.log("hi");
